8e53c2e4d161420b70c2f6497418b0e19658223e
Ruhisawant <ruhi.p.sawant@gmail.com>
2025-04-17 14:00:00 -0400

    Navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
 import CreateMember from './pages/CreateMember';
 import MemberGallery from './pages/MemberGallery';
 import MemberDetails from './components/MemberDetails';
@@ -8,19 +9,13 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1>F1 Team Manager</h1>
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/create" element={<CreateMember />} />
-            <Route path="/gallery" element={<MemberGallery />} />
-            <Route path="/car/:id" element={<MemberDetails />} />
-          </Routes>
-        </main>
-      </div>
+      <Navbar />    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<CreateMember />} />
+        <Route path="/gallery" element={<MemberGallery />} />
+        <Route path="/car/:id" element={<MemberDetails />} />
+      </Routes>
     </Router>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,52 @@
-import { Link, useLocation } from 'react-router-dom'
-import './Navbar.css'
+import { Link, useLocation } from 'react-router-dom';
+import { House, UsersRound, Plus, Trophy, Settings } from 'lucide-react';
+import './Navbar.css';
 
 function Navbar() {
-  const location = useLocation()
-  
+  const location = useLocation();
+
   const isActive = (path) => {
-    return location.pathname === path ? 'nav-link-active' : 'nav-link'
-  }
+    return location.pathname === path ? 'nav-link-active' : 'nav-link';
+  };
 
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-logo">
-            F1 Team Manager
-          </Link>
-        </div>
-        
+      <div className="navbar-logo">
+        <div className="symbol">F1</div>
+        <span className="logo-text">Box Box Box</span>
+      </div>
+
         <ul className="navbar-menu">
           <li className="navbar-item">
             <Link to="/" className={isActive('/')}>
-              Home
+              <House className="icon" /> Home
             </Link>
           </li>
           <li className="navbar-item">
             <Link to="/create" className={isActive('/create')}>
-              Create Car
+              <Plus className="icon" /> Add Member
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/gallery" className={isActive('/gallery')}>
+              <UsersRound className="icon" /> Team
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/performance" className={isActive('/performance')}>
+              <Trophy className="icon" /> Performance
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/settings" className={isActive('/settings')}>
+              <Settings className="icon" /> Settings
             </Link>
           </li>
         </ul>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
